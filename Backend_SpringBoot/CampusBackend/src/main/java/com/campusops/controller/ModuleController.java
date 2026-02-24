086461f338836b6a8c88929391e5e0def604638c
Aditya Sabale <sabale.aditya@gmail.com>
2026-02-24 22:36:15 +0530

    module controller added

---
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/controller/ModuleController.java
@@ -0,0 +1,52 @@
+package com.sunbeam.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sunbeam.models.ModuleDTO;
+import com.sunbeam.services.ModuleService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/modules")
+public class ModuleController {
+
+	@Autowired private ModuleService module;
+	
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody ModuleDTO dto) {		
+		module.saveModule(dto);
+		System.out.println(dto);
+		return ResponseEntity.ok("Module registered successfully");
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> findAll() {	
+	System.out.println(module.listall());
+		return ResponseEntity.ok(module.listall());
+	}
+	
+	@GetMapping("course/{id}")
+	public ResponseEntity<?> findAll(@PathVariable("id") int id) {		
+		return ResponseEntity.ok(module.listByCourse(id));
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<?> findById(@PathVariable("id") int id) {		
+		return ResponseEntity.ok(module.findById(id));
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<?> deleteById(@PathVariable("id") int id) {
+		module.deleteModule(id);
+		return ResponseEntity.ok("Deleted successfully");
+	}
+}
