eb530a69bf7ee7da7642f1345a77efa4e997400c
Aditya Sabale <sabale.aditya@gmail.com>
2026-02-25 11:44:23 +0530

    QuestionReportDto added

---
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/models/QuestionReportDTO.java
@@ -0,0 +1,10 @@
+package com.campusops.models;
+public class QuestionReportDTO {
+    public Integer questionId;
+    public String questionText;   // ADD THIS
+    public long excellent;
+    public long good;
+    public long satisfactory;
+    public long poor;
+    public double average;
+}
