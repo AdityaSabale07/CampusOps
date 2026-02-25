c0fb5a1c77fa3f4b518280573e33a681247cc5d5
Aditya Sabale <sabale.aditya@gmail.com>
2026-02-25 11:41:46 +0530

    QuestionDto added

---
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/models/QuestionListDTO.java
@@ -0,0 +1,25 @@
+package com.campusops.models;
+
+import java.util.List;
+
+public class QuestionListDTO {
+
+    private Integer templateId;
+    private List<String> questions;
+
+    public Integer getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(Integer templateId) {
+        this.templateId = templateId;
+    }
+
+    public List<String> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<String> questions) {
+        this.questions = questions;
+    }
+}
