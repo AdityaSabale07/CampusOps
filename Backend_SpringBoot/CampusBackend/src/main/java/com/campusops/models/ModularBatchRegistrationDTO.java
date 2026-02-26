38fcd0dfd7d6705b449ab5d87e94e5c79fc3de87
Aditya Sabale <sabale.aditya@gmail.com>
2026-02-26 11:23:52 +0530

    ModularBatchRegistrationDTO added

---
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/models/ModularBatchRegistrationDTO.java
@@ -0,0 +1,51 @@
+package com.campusops.models;
+
+public class ModularBatchRegistrationDTO {
+
+    private String studentName;
+    private String email;
+    private String phone;
+    private Integer batchId;
+    private Integer discountId;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(Integer batchId) {
+        this.batchId = batchId;
+    }
+
+	public Integer getDiscountId() {
+		return discountId;
+	}
+
+	public void setDiscountId(Integer discountId) {
+		this.discountId = discountId;
+	}
+    
+}
