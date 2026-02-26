b3f16ee13f53091ca263c0e8e4323e03f3c0caf2
Aditya Sabale <sabale.aditya@gmail.com>
2026-02-26 12:16:07 +0530

    ComboBatchDiscountStrategy class  added

---
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/discount/ComboBatchDiscountStrategy.java
@@ -0,0 +1,35 @@
+package com.campusops.discount;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.campusops.daos.ModularBatchRegistrationRepository;
+import com.campusops.entities.Discount;
+import com.campusops.entities.ModularBatchRegistration;
+
+@Component("COMBO")
+public class ComboBatchDiscountStrategy implements DiscountStrategy {
+
+    @Autowired
+    private ModularBatchRegistrationRepository regRepo;
+
+    @Override
+    public double applyDiscount(
+            ModularBatchRegistration reg,
+            Discount discount) {
+
+        // check if student already completed
+        boolean hasPrevious =
+                regRepo.existsByEmailAndStatus(
+                        reg.getEmail(),
+                        "APPROVED");
+
+        if (hasPrevious) {
+
+            return reg.getOriginalFee()
+                    * discount.getValue() / 100;
+        }
+
+        return 0;
+    }
+}
