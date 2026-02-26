53eae453056e1d12bbed3e8423cd11de1c8db62b
Aditya Sabale <sabale.aditya@gmail.com>
2026-02-26 11:54:13 +0530

    DiscountAnalyticsDTO added

---
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/models/DiscountAnalyticsDTO.java
@@ -0,0 +1,53 @@
+package com.campusops.models;
+
+public class DiscountAnalyticsDTO {
+
+    private String discountName;
+    private String discountType;
+
+    private long usageCount;
+    private double totalDiscount;
+    private double totalRevenue;
+
+    // ===== GETTERS & SETTERS =====
+
+    public String getDiscountName() {
+        return discountName;
+    }
+
+    public void setDiscountName(String discountName) {
+        this.discountName = discountName;
+    }
+
+    public String getDiscountType() {
+        return discountType;
+    }
+
+    public void setDiscountType(String discountType) {
+        this.discountType = discountType;
+    }
+
+    public long getUsageCount() {
+        return usageCount;
+    }
+
+    public void setUsageCount(long usageCount) {
+        this.usageCount = usageCount;
+    }
+
+    public double getTotalDiscount() {
+        return totalDiscount;
+    }
+
+    public void setTotalDiscount(double totalDiscount) {
+        this.totalDiscount = totalDiscount;
+    }
+
+    public double getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    public void setTotalRevenue(double totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+}
