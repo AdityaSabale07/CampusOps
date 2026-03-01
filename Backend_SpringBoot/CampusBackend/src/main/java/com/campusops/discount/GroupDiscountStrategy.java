161a47acc29a423ad5b6632932c2bc34c81273f0
Aditya Sabale <sabale.aditya@gmail.com>
2026-03-01 12:27:35 +0530

    GroupDiscountStratergy updated

--- Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/discount/GroupDiscountStrategy.java
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/discount/GroupDiscountStrategy.java
@@ -1,32 +1,21 @@
 package com.campusops.discount;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.campusops.daos.ModularBatchRegistrationRepository;
 import com.campusops.entities.Discount;
 import com.campusops.entities.ModularBatchRegistration;
 
 @Component("GROUP")
 public class GroupDiscountStrategy implements DiscountStrategy {
 
-    @Autowired
-    private ModularBatchRegistrationRepository regRepo;
-
     @Override
     public double applyDiscount(
             ModularBatchRegistration reg,
             Discount discount) {
 
-        long count =
-                regRepo.countByBatchId(
-                        reg.getBatch().getId());
+        // Group eligibility handled by DiscountEngine
+        // (studentEmail + batch + dates)
 
-        if (count >= 3) {
-            return reg.getOriginalFee()
-                    * discount.getValue() / 100;
-        }
-
-        return 0;
+        return 1; // eligible
     }
 }
