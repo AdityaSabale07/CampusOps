fd812d2e60340d64f59d68ce60a73997745b164b
Aditya Sabale <sabale.aditya@gmail.com>
2026-03-01 12:41:29 +0530

    Updated DiscountDto.java

--- Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/models/DiscountDTO.java
+++ Backend_SpringBoot/CampusBackend/src/main/java/com/campusops/models/DiscountDTO.java
@@ -7,6 +7,7 @@ public class DiscountDTO {
     private int id;
     private String name;
     private String type;
+    private String description;
     private double value;
     private String mode;
     private LocalDate startDate;
@@ -16,65 +17,82 @@ public class DiscountDTO {
 
     public DiscountDTO() {}
 
-    public DiscountDTO(int id, String name, String type,
-                       double value,
-                       LocalDate startDate,
-                       LocalDate endDate) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.value = value;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
+	public DiscountDTO(int id, String name, String type, String description, double value, String mode,
+			LocalDate startDate, LocalDate endDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.description = description;
+		this.value = value;
+		this.mode = mode;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
 
-    // ===== Getters & Setters =====
+	public int getId() {
+		return id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public double getValue() {
-        return value;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setValue(double value) {
-        this.value = value;
-    }
+	public double getValue() {
+		return value;
+	}
 
-    public LocalDate getStartDate() {
-        return startDate;
-    }
+	public void setValue(double value) {
+		this.value = value;
+	}
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
+	public String getMode() {
+		return mode;
+	}
 
-    public LocalDate getEndDate() {
-        return endDate;
-    }
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
 
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+ 
 }
